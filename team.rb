17a9e030418d8795218d365e71a0eaf138e435ca
daiya.shiratori <daiyashiratori@gmail.com>
2021-09-12 11:29:03 +0000

    勝敗

--- team.rb
+++ team.rb
@@ -13,19 +13,27 @@ class Team
 
   # インスタンスが持つメソッド（処理）
   def calc_win_rate
-    puts self.win.to_f / (self.win + self.lose)
+    self.win.to_f / (self.win + self.lose)
     
   end
   
   def show_team_result
-      puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は0.{calc_win_rate}です。"
+      puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate}です。"
   end
 end
 
-# インスタンスの生成と、変数への代入
 team1 = Team.new("Giants", 67, 45, 8)
+team2 = Team.new("Tigers", 60, 53, 7)
+team3 = Team.new("Dragons", 60, 55, 5)
+team4 = Team.new("BeyStars", 56, 58, 6)
+team5 = Team.new("Carp", 52, 56, 12)
+team6 = Team.new("Swallows", 41, 69, 10)
 
 
-# インスタンスの使用
 team1.show_team_result
+team2.show_team_result
+team3.show_team_result
+team4.show_team_result
+team5.show_team_result
+team6.show_team_result
 
